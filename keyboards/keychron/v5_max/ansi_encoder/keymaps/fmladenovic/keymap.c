f46aef6250e6e44e7a118342e8b056dc4903a90e
Fedja Mladenovic <fmladenovic@microsoft.com>
2025-01-23 19:38:43 +0100

    test for tap dance
    tap caps             -> quote
    hold caps            -> switch to layer 0 while held
    2x tap caps          -> comma
    double tap hold caps -> switch to layer 1 while held

--- keyboards/keychron/v5_max/ansi_encoder/keymaps/fmladenovic/keymap.c
+++ keyboards/keychron/v5_max/ansi_encoder/keymaps/fmladenovic/keymap.c
@@ -23,6 +23,7 @@ enum layers {
     WIN_BASE,
     WIN_FN,
     MY_LAYER_0,
+    MY_LAYER_1,
 };
 
 // ----------------------- Macros ------------------------
@@ -63,9 +64,43 @@ const key_override_t **key_overrides = (const key_override_t *[]){
     NULL
 };
 
+// ---------------------- Tap Dance ----------------------
+
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_TAP,
+    TD_DOUBLE_HOLD
+} td_state_t;
+
+typedef struct {
+    bool is_press_action;
+    td_state_t state;
+} td_tap_t;
+
+enum tap_dances { // custom tap dance keys
+    CAPS_LYRS,
+};
+
+
+td_state_t cur_dance(tap_dance_state_t *state);
+
+void ql_finished(tap_dance_state_t *state, void *user_data);
+void ql_reset(tap_dance_state_t *state, void *user_data);
+
+// associate our tap dance key with its functionality
+tap_dance_action_t tap_dance_actions[] = {
+    [CAPS_LYRS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ql_finished, ql_reset)
+};
+
 // ----------------------- Key Maps ----------------------
 
 const int LAYER0 = MO(MY_LAYER_0);
+const int CLEFT = LCTL(KC_LEFT);
+const int CRIGHT = LCTL(KC_RGHT);
+const int TD_CAPS = TD(CAPS_LYRS);
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -87,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,     KC_F11,   KC_F12,             KC_DEL,   KC_HOME,  KC_END,     KC_MUTE,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_NUM,   KC_PSLS,  KC_PAST,    KC_PMNS,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_P7,    KC_P8,    KC_P9,      KC_PPLS,
-        LAYER0,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_P4,    KC_P5,    KC_P6,
+        TD_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_P4,    KC_P5,    KC_P6,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,    KC_P1,    KC_P2,    KC_P3,      KC_PENT,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT            ),
     [WIN_FN] = LAYOUT_ansi_98(
@@ -104,6 +139,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,  KC_DEL,   _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,              _______,            _______,  _______,  _______,
         _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,  _______,  _______,  _______,  _______,    _______,
         _______,  _______,  _______,                                SPACE4,                                 _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______            ),
+    [MY_LAYER_1] = LAYOUT_ansi_98(
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,  _______,  _______,    _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,  _______,  _______,    _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,  _______,  _______,    _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  CLEFT,    KC_END,   KC_HOME,  CRIGHT,   _______,  _______,              _______,            _______,  _______,  _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,  _______,  _______,  _______,  _______,    _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______            ),
  };
 
 #if defined(ENCODER_MAP_ENABLE)
@@ -113,6 +155,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [WIN_BASE]   = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [WIN_FN]     = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
     [MY_LAYER_0] = {ENCODER_CCW_CW(KC_NO, KC_NO)}, // no-op
+    [MY_LAYER_1] = {ENCODER_CCW_CW(KC_NO, KC_NO)}, // no-op
 };
 #endif // ENCODER_MAP_ENABLE
 
@@ -183,5 +226,54 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return false;
     }
     return true;
+}
+
+// determine the current tap dance state
+td_state_t cur_dance(tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (!state->pressed) return TD_SINGLE_TAP;
+        else return TD_SINGLE_HOLD;
+    } else if (state->count == 2) {
+        if (!state->pressed) return TD_DOUBLE_TAP;
+        else return TD_DOUBLE_HOLD;
+    }
+    else return TD_UNKNOWN;
+}
+
+// initialize tap structure
+static td_tap_t ql_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+// tap dance function
+void ql_finished(tap_dance_state_t *state, void *user_data) {
+    ql_tap_state.state = cur_dance(state);
+    switch (ql_tap_state.state) {
+        case TD_SINGLE_TAP:
+            tap_code(KC_QUOT);
+            break;
+        case TD_SINGLE_HOLD:
+            layer_on(MY_LAYER_0);
+            break;
+        case TD_DOUBLE_TAP:
+            tap_code(KC_COMMA);
+            break;
+        case TD_DOUBLE_HOLD:
+            layer_on(MY_LAYER_1);
+            break;
+        default:
+            break;
+    }
+}
+
+void ql_reset(tap_dance_state_t *state, void *user_data) {
+    // if the key was held down and now is released then switch off the layer
+    if (ql_tap_state.state == TD_SINGLE_HOLD) {
+        layer_off(MY_LAYER_0);
+    } else if (ql_tap_state.state == TD_DOUBLE_HOLD) {
+        layer_off(MY_LAYER_1);
+    }
+    ql_tap_state.state = TD_NONE;
 }
 
