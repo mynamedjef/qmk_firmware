d0883b530136d3e6ef09400c3f24f45cea037960
Fedja Mladenovic <fmladenovic@microsoft.com>
2025-03-05 14:30:37 +0100

    move rules and config to userspace

--- keyboards/keychron/v5_max/ansi_encoder/rules.mk
+++ keyboards/keychron/v5_max/ansi_encoder/rules.mk
@@ -1,6 +1,1 @@
 # This file intentionally left blank
-
-KEY_OVERRIDE_ENABLE = yes
-TAP_DANCE_ENABLE = yes
-LEADER_ENABLE = yes
-DYNAMIC_MACRO_ENABLE = yes
--- users/fmladenovic/rules.mk
+++ users/fmladenovic/rules.mk
@@ -1,7 +1,15 @@
 
 SRC += fmladenovic.c
+
+KEY_OVERRIDE_ENABLE = yes
 SRC += features/overrides.c
+
+TAP_DANCE_ENABLE = yes
 SRC += features/tap_dance.c
+
+LEADER_ENABLE = yes
 SRC += features/leader.c
+
+DYNAMIC_MACRO_ENABLE = yes
 
 #INTROSPECTION_KEYMAP_C = features/overrides.c
